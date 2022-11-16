c9b6b4da06eaed7d96772c604879a1889796b673
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-11-15 22:48:03 -0600

    config hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-deploy");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -11,6 +16,12 @@ module.exports = {
     localhost: {
       chainId: 31337,
     },
+    goerli: {
+      chainId: 5,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
   },
   namedAccounts: {
     deployer: {
@@ -18,6 +29,11 @@ module.exports = {
     },
     voter: {
       default: 1,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
     },
   },
   solidity: "0.8.7",
